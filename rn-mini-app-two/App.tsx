124386052f4fc8ef2a663fe209b63c912a814056
puneetsharma1991 <vkpuneetsharma@gmail.comgit config --global user.email vkpuneetsharma@gmail.com>
2023-10-26 21:32:13 +0530

    main commit

---
+++ rn-mini-app-two/App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.mainContainer}>
+          <Text>Mini Two One</Text>
+          <Button
+            title="Click Me Two One"
+            onPress={() => Alert.alert('App Two Title', 'App Two Message')}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FEF8DD',
+  },
+  scrollContainer: {
+    height: '100%',
+  },
+});
+
+export default App;
